7cbff2848425573d4d840a5740397bed30e5421b
ch393366 <crharper@fedex.com>
2019-01-05 21:05:20 -0700

    Initial Commit

---
+++ src/main/java/com/fedex/ics/CalculateTimeOfArrival.java
@@ -0,0 +1,14 @@
+package com.fedex.ics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateTimeOfArrival {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateTimeOfArrival.class, args);
+	}
+
+}
+
